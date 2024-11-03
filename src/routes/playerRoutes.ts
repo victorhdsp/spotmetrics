619dc42351c7ee40808b8c33a6b1009429713efa
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-03 17:14:05 -0300

    modificação no retorno da mensagem na rota.

--- src/routes/playerRoutes.ts
+++ src/routes/playerRoutes.ts
@@ -2,21 +2,24 @@ import { type Request, type Response, Router } from "express"
 import playerController from "../controllers/playerController";
 
 const route = Router();
-route.get("/", (req: Request, res: Response) => {
-	res.status(200).json({ message: "all content" });
+route.get("/", async (req: Request, res: Response) => {
+	const result = await playerController.getAll(req);
+	res.status(200).json(result);
 });
-route.get("/:id", (req: Request, res: Response) => {
-	res.status(200).json({ message: "unique content" });
+route.get("/:id", async (req: Request, res: Response) => {
+	const result = await playerController.get(req);
+	res.status(200).json(result);
 });
 route.post("/", async (req: Request, res: Response) => {
-	const user = await playerController.create(req);
-	res.status(201).json({ user });
+	const result = await playerController.create(req);
+	res.status(201).json(result);
 });
 route.delete("/:id", async (req: Request, res: Response) => {
 	await playerController.delete(req);
 	res.status(200).json({ message: "ok" });
 });
-route.put("/:id", (req: Request, res: Response) => {
-	res.status(200).json({ message: "changed" });
+route.put("/:id", async (req: Request, res: Response) => {
+	const result = await playerController.get(req);
+	res.status(200).json(result);
 });
 export default route;
