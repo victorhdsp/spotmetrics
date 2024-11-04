cb5af2d101eb59772a1b660615b068a0f31bd110
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-04 19:45:04 -0300

    adição asyncHandle para lidar com error handle em ambientes assincronos

--- src/routes/playerRoutes.ts
+++ src/routes/playerRoutes.ts
@@ -1,25 +1,32 @@
-import { type Request, type Response, Router } from "express"
+import { type Request, type Response, Router } from "express";
+import asyncHandler from "express-async-handler"
 import playerController from "../controllers/playerController";
 
 const route = Router();
-route.get("/", async (req: Request, res: Response) => {
+
+route.get("/", asyncHandler(async (req: Request, res: Response) => {
 	const result = await playerController.getAll(req);
 	res.status(200).json(result);
-});
-route.get("/:id", async (req: Request, res: Response) => {
+}));
+
+route.get("/:id", asyncHandler(async (req: Request, res: Response) => {;
 	const result = await playerController.get(req);
 	res.status(200).json(result);
-});
-route.post("/", async (req: Request, res: Response) => {
+}));
+
+route.post("/", asyncHandler(async (req: Request, res: Response) => {
 	const result = await playerController.create(req);
 	res.status(201).json(result);
-});
-route.delete("/:id", async (req: Request, res: Response) => {
+}));
+
+route.delete("/:id", asyncHandler(async (req: Request, res: Response) => {
 	await playerController.delete(req);
 	res.status(200).json({ message: "ok" });
-});
-route.put("/:id", async (req: Request, res: Response) => {
-	const result = await playerController.get(req);
+}));
+
+route.put("/:id", asyncHandler(async (req: Request, res: Response) => {
+	const result = await playerController.change(req);
 	res.status(200).json(result);
-});
+}));
+
 export default route;
