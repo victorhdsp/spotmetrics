50cda357e7f60e38eca7b8e2b8bc16bc7e10edda
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-04 19:41:46 -0300

    remoção do try-catch para error handle e correção de bugs

--- src/model/playerModel/changePlayer.ts
+++ src/model/playerModel/changePlayer.ts
@@ -1,43 +1,59 @@
+import type { Prisma } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
 import prisma from "../../../prisma/database";
-import { DefaultArgs } from "@prisma/client/runtime/library";
-import { Prisma } from "@prisma/client";
-import { PlayerChangeInput } from "../../utils/types";
+import type { PlayerChangeInput } from "../../utils/types/player";
 
 type Data = PlayerChangeInput;
-type Output = Prisma.Prisma__PlayerClient<{
-    id: string;
-    name: string;
-    username: string | null;
-    createdAt: Date;
-    skills: {
-        id: string;
-        power: number;
-        speed: number;
-        dribble: number | null;
-        playerId: string;
-    };
-}, never, DefaultArgs>;
+type Output = Prisma.Prisma__PlayerClient<
+	{
+		id: string;
+		name: string;
+		username: string | null;
+		createdAt: Date;
+		skills: {
+			id: string;
+			power: number;
+			speed: number;
+			dribble: number | null;
+			playerId: string;
+		};
+	},
+	never,
+	DefaultArgs
+>;
 
 function changePlayer(id: string, data: Data): Output {
-    try {
-        const player = prisma.player.update({
-            where: {
-                id
-            },
-            data: data,
-            select: {
-                id: true,
-                name: true,
-                createdAt: true,
-                username: true,
-                skills: true
-            }
-        })
-        return player;
-    } catch (error) {
-        const err: Error = error as any;
-        throw new Error(err.message);
-    }
+	try {
+		const player = prisma.player.update({
+			where: {
+				id,
+			},
+			data: {
+				id: data?.id,
+				name: data?.name,
+				username: data?.username,
+				createdAt: data?.createdAt,
+				skills: {
+					update: {
+						power: data?.skills?.power,
+						speed: data?.skills?.speed,
+						dribble: data?.skills?.dribble,
+					},
+				}
+			},
+			select: {
+				id: true,
+				name: true,
+				createdAt: true,
+				username: true,
+				skills: true,
+			},
+		});
+		return player;
+	} catch (error) {
+		const err: Error = error as any;
+		throw new Error(err.message);
+	}
 }
 
 export default changePlayer;
