7cd006c4d0011bf07f5bf71b04a2c919e8f94a64
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-04 19:40:50 -0300

    mudança no retorno de all players e remoção de alguns asyncs não necessários

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -1,40 +1,59 @@
+import type { Request } from "express";
+import type { AllPlayersParams } from "../model/playerModel/getAllPlayers";
 import playerService from "../service/playerService";
-import { Request } from "express";
-import { AllPlayersParams } from "../model/playerModel/getAllPlayers";
-import { PlayerComplete, PlayerResumed } from "../utils/types";
+import type { PlayerComplete, PlayerResumed } from "../utils/types/player";
 
-async function createPlayer(req: Request): Promise<PlayerComplete> {
-    const data = req.body;
-    return playerService.create(data);
+function createPlayer(req: Request): Promise<PlayerComplete> {
+	const data = req.body;
+	return playerService.create(data);
 }
-async function deletePlayer(req: Request): Promise<Boolean> {
-    const id = req.params.id;
-    return playerService.delete(id);
+
+function deletePlayer(req: Request): Promise<boolean> {
+	const id = req.params.id;
+	return playerService.delete(id);
 }
-async function changePlayer(req: Request): Promise<PlayerComplete> {
-    const id = req.params.id;
-    const data = req.body;
-    return playerService.change(id, data);
+
+function changePlayer(req: Request): Promise<PlayerComplete> {
+	const id = req.params.id;
+	const data = req.body;
+	return playerService.change(id, data);
 }
-async function getUniquePlayer(req: Request): Promise<PlayerComplete> {
-    const id = req.params.id;
-    return playerService.get(id);
+
+function getUniquePlayer(req: Request): Promise<PlayerComplete> {
+	const id = req.params.id;
+	return playerService.get(id);
 }
-async function getAllPlayers(req: Request): Promise<PlayerResumed[]> {
-    const params: AllPlayersParams = {
-        name: `${req.query.name}`,
-        username: `${req.query.username}`,
-        size: parseInt(`${req.query.size || 10}`),
-        page: parseInt(`${req.query.page || 1}`)
-    } 
-    return playerService.getAll(params);
+
+interface OutputGetAllPlayers {
+	players: PlayerResumed[];
+	total: number;
+	next: string | null;
+	prev: string | null;
+}
+async function getAllPlayers(req: Request): Promise<OutputGetAllPlayers> {
+	const params: AllPlayersParams = {
+		name: req.query.name ?`${req.query.name}` : undefined,
+		username: req.query.username ? `${req.query.username}` : undefined,
+		size: req.query.limit ? Number(`${req.query.limit}`) : undefined,
+		page: Number(`${req.query.offset || 1}`) ,
+	};
+	const { players, total } = await playerService.getAll(params);
+	const next = `/v1/players?limit=${params.size}&offset=${params.page + 1}`;
+	const prev = `/v1/players?limit=${params.size}&offset=${params.page - 1}`;
+	const size = params.size ? params.size : players.length;
+	return {
+		players,
+		total,
+		next: params.page * size < total ? next : null,
+		prev: params.page > 0 ? prev : null,
+	};
 }
 
 const playerController = {
-    create: createPlayer,
-    delete: deletePlayer,
-    change: changePlayer,
-    get: getUniquePlayer,
-    getAll: getAllPlayers
-}
+	create: createPlayer,
+	delete: deletePlayer,
+	change: changePlayer,
+	get: getUniquePlayer,
+	getAll: getAllPlayers,
+};
 export default playerController;
