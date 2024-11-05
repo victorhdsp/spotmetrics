529cf2753bea6ead472503378ff67e46fbaa3e2a
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-05 14:52:24 -0300

    install cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { innitialize } from "../prisma/database";
 import errorMiddleware from "./middleware/error";
 import routes from "./routes";
-import { startSwagger } from "./swagger";
 
 dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const corsOptions: cors.CorsOptions = {
+	origin: "*",
+	methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
 innitialize().then(() => {
 	const app = express();
+	app.use(cors(corsOptions));
 	app.use(express.json());
-	startSwagger(app, PORT);
 	app.use(routes);
 	app.use(errorMiddleware);
 	return app.listen(PORT, () => `server running on port ${PORT}`);
