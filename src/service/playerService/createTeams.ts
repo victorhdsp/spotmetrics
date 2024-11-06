d6d0051ce4002b4fb03534a258e835b5bb7d8e29
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-06 10:39:03 -0300

    correção na lógica quando acaba os jogadores não utilizados

--- src/service/playerService/createTeams.ts
+++ src/service/playerService/createTeams.ts
@@ -1,10 +1,12 @@
-import type { CreateTeamsParams, OutputCreateTeams } from "../../controllers/playerController/createTeams";
+import type {
+	CreateTeamsParams,
+	OutputCreateTeams,
+} from "../../controllers/playerController/createTeams";
 import playerModel from "../../model/playerModel";
 import type { PlayerComplete } from "../../utils/types/player";
 
 function getPlayerPower(player: PlayerComplete) {
-	if (!player || !player?.skills)
-		throw new Error("Erro inesperado.");
+	if (!player || !player?.skills) throw new Error("Erro inesperado.");
 	const { power, speed, dribble } = player.skills;
 	return power + speed + (dribble || 0);
 }
@@ -25,7 +27,7 @@ function randomPlayer(players: PlayerComplete[], size: number) {
 		const index = Math.floor(Math.random() * players.length);
 		const player = players[index];
 		players.splice(index, 1);
-		
+
 		if (team1.length < team2.length) {
 			team1.push(player);
 		} else {
@@ -35,49 +37,54 @@ function randomPlayer(players: PlayerComplete[], size: number) {
 	const team1Power = getTeamPower(team1);
 	const team2Power = getTeamPower(team2);
 	if (team1Power < team2Power) {
-		return { 
+		return {
 			lower: orderTeamByPower(team1),
 			higher: orderTeamByPower(team2),
-			unusedPlayers: orderTeamByPower(players)
+			unusedPlayers: orderTeamByPower(players),
 		};
 	}
-	return { 
+	return {
 		lower: orderTeamByPower(team2),
 		higher: orderTeamByPower(team1),
-		unusedPlayers: orderTeamByPower(players)
+		unusedPlayers: orderTeamByPower(players),
 	};
 }
 
-async function createTeams(params: Required<CreateTeamsParams>): Promise<OutputCreateTeams> {
-	const tradeCount = 5;
+async function createTeams(
+	params: Required<CreateTeamsParams>,
+): Promise<OutputCreateTeams> {
+	const tradeCount = params.size / 2;
 	let index = 0;
 	const players = await playerModel.getRanking();
 	if (players.length < params.size * 2)
 		throw new Error("Não há jogadores suficientes para criar um time.");
-	const { lower:bLower, higher, unusedPlayers } = randomPlayer(players, params.size);
+	const {
+		lower: bLower,
+		higher,
+		unusedPlayers,
+	} = randomPlayer(players, params.size);
 	const higherPower = getTeamPower(higher);
 	let lower = bLower;
-	
+
 	while (index < tradeCount) {
 		const lowerPower = getTeamPower(lower);
-		if (Math.abs(higherPower - lowerPower) < 10)
-			break;
+		if (Math.abs(higherPower - lowerPower) < 10) break;
 		if (unusedPlayers.length > 0) {
 			if (getPlayerPower(unusedPlayers[0]) < getPlayerPower(lower[0]))
 				unusedPlayers.shift();
 			lower[0] = unusedPlayers[0];
 			unusedPlayers.shift();
-			lower = orderTeamByPower(bLower)
+			lower = orderTeamByPower(bLower);
 		} else {
 			const swap: PlayerComplete = lower[index];
-			lower[index] = higher[tradeCount - index];
+			lower[index] = higher[(higher.length - 1) - index];
 			higher[index] = swap;
 			index++;
 		}
 	}
 	return {
 		team1: lower,
-		team2: higher
+		team2: higher,
 	};
 }
 
