e5841acfc4c5aec3a93c1b15032d89c7c61f0e52
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-11-06 09:42:20 -0300

    verificação do limite de jogadores

--- src/controllers/playerController/createTeams.ts
+++ src/controllers/playerController/createTeams.ts
@@ -1,6 +1,8 @@
 import type { Request } from "express";
+import { z } from "zod";
 import playerService from "../../service/playerService";
 import type { PlayerComplete } from "../../utils/types/player";
+import { zParse } from "../../utils/zod/zParse";
 
 export interface OutputCreateTeams {
 	team1: PlayerComplete[];
@@ -11,11 +13,16 @@ export interface CreateTeamsParams {
 	size?: number;
 }
 
+const CreateTeamsSchema = z.object({
+	size: z.number().min(5, "O time não pode ser menor que 5 jogadores por time.")
+}) satisfies z.ZodType<CreateTeamsParams>
+
 async function createTeams(req: Request): Promise<OutputCreateTeams> {
-	const params: CreateTeamsParams = {
-		size: req.query.limit ? Number(`${req.query.limit}`): undefined
+	const params: Required<CreateTeamsParams> = {
+		size: Number(`${req.query.limit || 5}`)
 	};
-	const players = await playerService.createTeams(params);
+	const resolvedParams = await zParse(CreateTeamsSchema, params);
+	const players = await playerService.createTeams(resolvedParams);
 	return players;
 }
 
